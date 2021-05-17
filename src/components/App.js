8587e06885a58e55d3213c9dce9bc2cc80b0e1c3
Kirill <kirill.petrov@quickms.fr>
2021-05-17 21:44:20 +0200

    add footer with form, add alert for quantity water or light

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import Banner from '../components/Banner';
-import ShoppingList from '../components/ShoppingList';
-import Cart from '../components/Cart';
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  return (<div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-    )
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+
+			{/* <Cart /> */}
+			<ShoppingList />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -8,15 +8,15 @@ function ShoppingList() {
 
     return (
         <div>
-            <ul>
-                {categories.map((cat) => (
-                    <li key={cat}>{cat}</li>
-                ))}
-            </ul>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
 
             <ul className='lmj-plant-list'>
                 {plantList.map(({id, cover, name, water, light}) => (
-                    <PlantItem id={id} cover={cover} name={name} water={water} light={light} />
+                    <PlantItem key={id} cover={cover} name={name} water={water} light={light} />
                 ))}
             </ul>
         </div>
